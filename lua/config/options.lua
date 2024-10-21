729a89b75ab88082efe003e8133b06a29f1f1684
Eva Lott <evalott100@gmail.com>
2024-10-21 08:10:46 +0100

    updated config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.rustaceanvim = {
+  server = {
+    cmd = function()
+      local mason_registry = require("mason-registry")
+      local ra_binary = mason_registry.is_installed("rust-analyzer")
+          -- This may need to be tweaked, depending on the operating system.
+          and mason_registry.get_package("rust-analyzer"):get_install_path() .. "/rust-analyzer"
+        or "rust-analyzer"
+      return { ra_binary } -- You can add args to the list, such as '--log-file'
+    end,
+  },
+}
